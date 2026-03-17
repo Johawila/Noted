4d53c4515aa1a3452fd74eb7862382274d74cf1e
Johawila <johan.wilander@medhelpcare.com>
2026-03-17 23:06:35 +0100

    Initial Commit

---
+++ Noted/NotedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotedApp.swift
+//  Noted
+//
+//  Created by Johan Wilander on 2026-03-17.
+//
+
+import SwiftUI
+
+@main
+struct NotedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
